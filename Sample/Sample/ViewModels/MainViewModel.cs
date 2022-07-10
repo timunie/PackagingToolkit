7ffa3f39f8a9f6b8702e0f2d1d993fd27495a066
timunie <tim.uniewski@gmail.com>
2022-07-10 14:27:03 +0200

    Add minimal Sample

---
+++ Sample/Sample/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Sample.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string? _Greeting = "Hello {0}";
+
+        public string? Greeting
+        {
+            get => _Greeting;
+            set => SetProperty(ref _Greeting, value);
+        }
+
+
+        private string? _Name;
+        public string? Name
+        {
+            get => _Name; 
+            set
+            {
+                if (SetProperty(ref _Name, value))
+                {
+                    // If the name changed, notify our result changed as well. 
+                    OnPropertyChanged(nameof(Result));
+                }
+            }
+        }
+
+        public string Result => string.Format(Greeting ?? "{0}", Name);
+    }
+}
